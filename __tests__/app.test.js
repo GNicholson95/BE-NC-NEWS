74f14596a0866b77e9532e84a4ca09f301310dde
George Nicholson <ga.nicholson@outlook.com>
2023-10-02 15:16:45 +0100

    task 2 GET /api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,38 @@
+const app = require('../app');
+const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require('../db/data/test-data/index')
+
+beforeEach(() => {
+	return seed(data);
+});
+afterAll(() => db.end());
+
+describe('GET /api/topics', () => {
+    it("checks a good connection to api", () => {
+          return request(app).get("/api/topics").expect(200)
+      });
+      it("get all topics", () => {
+        return request(app).get("/api/topics").then((response) => {
+                expect(response.body.topics.length).toBe(3);
+             });
+         });
+      it("sends an array of topics of the correct format", () => {
+          return request(app).get("/api/topics").then((response) => {
+            response.body.topics.forEach((topic) => {
+                expect (topic.hasOwnProperty('slug')) .toBe(true);
+                expect (topic.hasOwnProperty('description')) .toBe(true)
+                expect(typeof topic.description).toBe("string");
+                expect(typeof topic.slug).toBe("string");
+                  });
+              });
+      });
+      it('should respond not found for invalid endpoint', () => {
+        return request(app).get('/api/invalid-endpoint')
+        .expect(404)
+        .then((response) => {
+            expect(response.body.msg).toBe('Not Found')
+        })
+    });
+  });
