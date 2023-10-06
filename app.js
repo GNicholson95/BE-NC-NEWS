c36b4256be900eb7edc1fb1dbd5dc3e1c992f62e
George Nicholson <ga.nicholson@outlook.com>
2023-10-06 09:11:04 +0100

    added missing line of code

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ app.get('/api', getEndpoints);
 
 app.get('/api/articles/:article_id', getArticleById);
 
+app.get('/api/articles', getAllArticles);
+
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 
-app.get('/api/articles', getAllArticles);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
