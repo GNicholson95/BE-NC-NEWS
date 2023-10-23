3cc3c28b5ccb893939d7d793b62be2dcd32f141e
George Nicholson <ga.nicholson@outlook.com>
2023-10-23 11:46:03 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const { getTopics, getEndpoints, getArticleById, getAllArticles, getCommentsByArticleId, postComment, getAllUsers, deleteComment, patchArticleById} = require('./controllers/controller'); 
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
